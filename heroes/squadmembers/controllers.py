0f2e4d20a7086a92035f410fede1e278f2a8b764
Mykola Kharechko <crchemist@gmail.com>
2015-11-02 22:15:34 +0200

    code cleanup.

--- heroes/squadmembers/controllers.py
+++ heroes/squadmembers/controllers.py
@@ -1,80 +1,75 @@
+import logging
+
 from flask import Blueprint, render_template, redirect, request
 
 from google.appengine.ext import ndb
 
-import logging
 
-from .models import Squadmember
+from heroes.positions.models import Position
 from heroes.representatives.models import Rep
 from heroes.roles.models import Role
-from heroes.positions.models import Position
+
+from .models import Squadmember
 
 squadmember_bp = Blueprint('squadmember', __name__)
 
-# RENDERING #
 
-# A squadmember PAGE.
 @squadmember_bp.route('/<key>/')
 def squadmember_view(key):
-	squadmember_key = ndb.Key(urlsafe=key)
-	squadmember = squadmember_key.get()
+    """A squadmember PAGE."""
+    squadmember_key = ndb.Key(urlsafe=key)
+    squadmember = squadmember_key.get()
 
+    #BREADCRUMB
+    squad = squadmember_key.parent().get()
+    team = squad.key.parent().get()
+    country = squad.key.parent().parent().get()
+    sport = squad.key.parent().parent().parent().get()
 
+    breadcrumb_list = [sport, country, team, squad]
+    title = squadmember.title
+    #END BREADCRUMB
 
-	#BREADCRUMB
-	# squad
-	squad = squadmember_key.parent().get()
-	#team
-	team = squad.key.parent().get()
-	# country
-	country = squad.key.parent().parent().get()
-	# sport
-	sport = squad.key.parent().parent().parent().get()
+    role_entries = Role.query(ancestor=country.key).fetch()
+    position_entries = Position.query(ancestor=country.key).fetch()
 
-	breadcrumb_list = [sport, country, team, squad]
-	title = squadmember.title
-	#END BREADCRUMB
-
-	role_entries = Role.query(ancestor=country.key).fetch()
-	position_entries = Position.query(ancestor=country.key).fetch()
-
-	return render_template('squadmember.html',
-		breadcrumb = breadcrumb_list,
-		object_title=title,
-		squadmember_object=squadmember,
-		squad_object=squad,
-		roles=role_entries,
-		positions=position_entries,
-	)
+    return render_template('squadmember.html',
+        breadcrumb = breadcrumb_list,
+        object_title=title,
+        squadmember_object=squadmember,
+        squad_object=squad,
+        roles=role_entries,
+        positions=position_entries,
+    )
 
 #NEW squadmember PAGE
 #---- not doing this yet
 # @squadmember_bp.route('/new/<key>')
 # def new_squadmember(key):
-# 	squad_key = ndb.Key(urlsafe=key)
-# 	squad = squad_key.get()
+#   squad_key = ndb.Key(urlsafe=key)
+#   squad = squad_key.get()
 
-# 	# COUNTRY > TEAM > SQUAD = 2 x parent
-# 	rep_entries = Rep.query(ancestor=squad_key.parent().parent()).fetch()
+#   # COUNTRY > TEAM > SQUAD = 2 x parent
+#   rep_entries = Rep.query(ancestor=squad_key.parent().parent()).fetch()
 
-# 	#BREADCRUMB
-# 	# squad - above
-# 	#team
-# 	team = squad.key.parent().get()
-# 	# country
-# 	country = squad.key.parent().parent().get()
-# 	# sport
-# 	sport = squad.key.parent().parent().parent().get()
+#   #BREADCRUMB
+#   # squad - above
+#   #team
+#   team = squad.key.parent().get()
+#   # country
+#   country = squad.key.parent().parent().get()
+#   # sport
+#   sport = squad.key.parent().parent().parent().get()
 
-# 	breadcrumb_list = [sport, country, team, squad]
-# 	#END BREADCRUMB
+#   breadcrumb_list = [sport, country, team, squad]
+#   #END BREADCRUMB
 
 
-# 	return render_template('squadmember.html',
-# 		object_title='Add squad members',
-# 		squad_object=squad,
-# 		reps = rep_entries,
-# 	)
+#   return render_template('squadmember.html',
+#       object_title='Add squad members',
+#       squad_object=squad,
+#       reps = rep_entries,
+#   )
 
 
 
@@ -83,42 +78,34 @@ def squadmember_view(key):
 # ADD squadmember
 @squadmember_bp.route('/add/<squad_key>/<rep_key>', methods=['GET'])
 def add_entry(squad_key, rep_key):
-	squad_key = ndb.Key(urlsafe=squad_key)
-	rep_key = ndb.Key(urlsafe=rep_key)
+    squad_key = ndb.Key(urlsafe=squad_key)
+    rep_key = ndb.Key(urlsafe=rep_key)
 
-	squadmember = Squadmember(rep=rep_key, parent=squad_key)
-	squadmember.put()
+    squadmember = Squadmember(rep=rep_key, parent=squad_key)
+    squadmember.put()
 
-	return redirect('/admin/squad/{}'.format(squad_key.urlsafe()))
+    return redirect('/admin/squad/{}'.format(squad_key.urlsafe()))
 
 
 # UPDATE squadmember
 @squadmember_bp.route('/update/<key>', methods=['POST'])
 def update_entry(key):
 
-	squadmember_key = ndb.Key(urlsafe=key)
-	squadmember = squadmember_key.get()
+    squadmember_key = ndb.Key(urlsafe=key)
+    squadmember = squadmember_key.get()
 
-	if request.form['roleinput']:
-		role_key = ndb.Key(urlsafe=request.form['roleinput'])
-		squadmember.role = role_key
+    if request.form['roleinput']:
+        role_key = ndb.Key(urlsafe=request.form['roleinput'])
+        squadmember.role = role_key
 
-	if request.form['positioninput']:
-		position_key = ndb.Key(urlsafe=request.form['positioninput'])
-		squadmember.position = position_key
-
-
-	squadmember.put()
-
-	return redirect('/admin/squadmember/{}'.format(squadmember.key.urlsafe()))
+    if request.form['positioninput']:
+        position_key = ndb.Key(urlsafe=request.form['positioninput'])
+        squadmember.position = position_key
 
 
+    squadmember.put()
+    photo = request.files.get('photo')
+    if photo:
+        pass
 
-
-
-
-
-
-
-
-
+    return redirect('/admin/squadmember/{}'.format(squadmember.key.urlsafe()))
